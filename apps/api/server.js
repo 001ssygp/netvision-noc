68b78a4c7d3c597353eea578efa3c445dafa8aec
001ssygp <62508126+001ssygp@users.noreply.github.com>
2026-07-13 13:34:34 +0200

    Set up basic Express server for API

--- apps/api/server.js
+++ apps/api/server.js
@@ -1,1 +1,24 @@
+const express=require("express");
 
+const app=express();
+
+app.get("/",(req,res)=>{
+
+res.json({
+
+name:"NetVision NOC API",
+
+version:"0.1.0"
+
+});
+
+});
+
+
+app.listen(4000,()=>{
+
+console.log(
+"API running on port 4000"
+);
+
+});
